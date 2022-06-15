66a78575afeeb4e247744e04e4cd52396375a079
ruiyeclub <ruiyeclub@foxmail.com>
2022-06-15 11:15:15 +0800

    feat: MyBatis-Plus

---
+++ src/main/java/cn/ruiyeclub/novel/core/config/MybatisPlusConfig.java
@@ -0,0 +1,28 @@
+package cn.ruiyeclub.novel.core.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 跨域配置
+ *
+ * @author: Cr.
+ * @date: 2022/6/15
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
